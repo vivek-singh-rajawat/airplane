117513bc8da7e409d5a14395930a11dd790a94bc
vivek singh <vs6651407@gmail.com>
2024-03-08 18:02:41 +0530

    Added routes

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
+const ApiRoutes = require('./routes/index');
 
 const setupAndStartServer = async() =>{
     
     //create the express object
     const app =  express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+
+    app.use('/api', ApiRoutes);
 
     app.listen(PORT, ()=>{
         console.log(`server is started ${PORT}`)
